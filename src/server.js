30d9be76e37ce0888d83323fdaf5eb6a3be1156f
ander <anderperez15@gmail.com>
2018-06-27 17:18:51 -0400

    add api

--- src/server.js
+++ src/server.js
@@ -2,17 +2,51 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config';
+import logger from 'morgan';
+import bodyParser from'body-parser';
+import path from 'path';
+import request from 'request';
+import redis from 'redis';
 
+//config redis
+const client = redis.createClient();
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+// initializing packages
 const app = express();
+//functions
+const dataCriptocoins = () => {
+  request('https://api.coinmarketcap.com/v2/ticker/?start=1&limit=1000&sort=id&structure=array', (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    if(response.statusCode) client.set('data', body);    
+  });
+};
 
-app.set("port", process.env.PORT || 3000);
+// settings
+app.set('port', process.env.PORT || 3000);
 
+// middlwares
 app.use(webpackDevMiddleware(webpack(webpackConfig)));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './src/client/public')));
 
-app.get('/',(req, res) => {
-  res.send('Hello word')
+// routes
+
+app.get('/api', (req, res) => {
+  client.get('data', (err, data) => {
+    res.json(JSON.parse(data)).status(200);
+  });
 });
 
-app.listen(app.get("port"),() => {
-  console.log("listen port",app.get("port"))
+// starting the server
+
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
 });
+
+dataCriptocoins();
+setInterval(dataCriptocoins, 10*60*1000);
